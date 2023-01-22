d44091f5c0a51e5d2ab327b3c663f1faffb1623c
Bret Belgarde <bretbelgarde@gmail.com>
2023-01-22 12:09:22 -0500

    Update data object, type hinting/return types

--- src/SkullBox/Models/Pastie.php
+++ src/SkullBox/Models/Pastie.php
@@ -1,53 +1,50 @@
 <?php
   namespace SkullBox\Models;
-  
+
+use DateTime;
+
   class Pastie {
-    private const KEYLENGTH = 8;
-
-    private $timePosted;
-    private $id;
-    private $content;
-    private $contentType;
-    private $visibility;
-
-    public function __construct($content, $contentType, \DateTime $timePosted = null, $id = null, $visibility = false) {
-      $this->timePosted = (is_null($timePosted)) ? new \DateTime() : $timePosted;
-      $this->id = (is_null($id)) ? uniqid('') : $id;
-      $this->content($content);
-      $this->contentType($contentType);
-      $this->visibility($visibility);
+     public function __construct(
+        private ?string $id = null,
+        private ?\DateTime $timePosted = null,
+        private string $content, 
+        private string $contentType, 
+        private bool $visible = true
+    ) {
+      $this->timePosted ?? new \DateTime();
+      $this->id ?? uniqid('');
     }
 
-    public function id() {
+    public function id() : string {
       return $this->id;
     }
 
-    public function timePosted() {
+    public function timePosted() : DateTime {
       return $this->timePosted;
     }
 
-    public function content($content = null) {
+    public function content(?string $content = null) {
       if (is_null($content)) return $this->content;
       $this->content = $content;
     }
 
-    public function contentType($contentType = null) {
+    public function contentType(?string $contentType = null) {
       if (is_null($contentType)) return $this->contentType;
       $this->contentType = $contentType;
     }
 
-    public function visibility($visibility = null) {
-      if (is_null($visibility)) return $this->visibility;
-      $this->visibility = $visibility;
+    public function visible(?string $visible = null) {
+      if (is_null($visible)) return $this->visible;
+      $this->visible = $visible;
     }
 
-    public function toArray() {
+    public function toArray() : array {
       return [
         'id'           => $this->id(),
         'content_type' => $this->contentType(),
         'content'      => $this->content(),
         'time_posted'  => $this->timePosted(),
-        'visibility'   => $this->visibility()
+        'visible'      => $this->visible()
       ];
     }
   }
