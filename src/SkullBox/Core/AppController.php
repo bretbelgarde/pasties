844bad2023f9feb512d642a9a7fd2ed9e0341893
Bret Belgarde <bretbelgarde@gmail.com>
2023-01-24 15:14:15 -0500

    Add get and add route handlers

--- src/SkullBox/Core/AppController.php
+++ src/SkullBox/Core/AppController.php
@@ -2,30 +2,90 @@
 
 namespace SkullBox\Core;
 
+use Base;
+
+use SkullBox\Core\PastieController;
+use SkullBox\Models\Pastie;
+
 class AppController
 {
+
   use \SkullBox\Utils\PageRenderer;
+  use \SkullBox\Utils\JsonRenderer;
 
-  protected object $app;
+  const VALID_FIELDS = [
+    'pastie_title',
+    'pastie_content',
+    'pastie_content_type'
+  ];
 
-  public function __construct(\Base $app) {
-      $this->app = $app;
+  private PastieController $pastieData;
+
+  public function __construct(private Base $app) {
+    $this->pastieData = new PastieController($app); 
   }
 
   public function index():void {
-    $data = ['message' => 'Hello World!'];
-    $this->render('home', $data);
+     $this->render('home');
   }
 
-  public function add():bool {
-    return true;
+  public function getPasties() : void {
+    $id ??= $this->app->get('PARAMS.id');
+    
+    if (!is_null($id)) {
+      $data = [];
+      $data[] = $this->pastieData->getOne($id)->toArray();
+    } else {
+      $data = array_map(function($pastie) {
+        return $pastie->toArray();
+      }, $this->pastieData->getAll());
+    }
+
+    $this->renderJSON($data);
   }
 
-  public function delete():bool {
-    return true;
+  public function addPastie() {
+    /*
+     * NOTE must use file_get_contents('php://input') to get
+     * json from an AJAX request.
+     */
+
+    $post = json_decode(file_get_contents('php://input'), true);
+
+    if ($this->validateFormData($post)) {
+      $this->renderJSON([
+        "message" => "Error: Invalid Entries in Post",
+        "error" => "The POST array contains entries that do not match allowed keys"
+      ]);
+      
+      return;
+    }
+    
+    try {
+      $this->pastieData->save(new Pastie(
+          $post['pastie_title'], 
+          $post['pastie_content'], 
+          $post['pastie_content_type']
+      ));
+    } catch (\Exception $e) {
+      $this->renderJSON([
+        "message" => "Error: Unable to Save Pastie",
+        "error"   => "{$e}"
+      ]);
+    }
+   
+    $this->renderJSON(["message" => "Pastie Added"]);
   }
 
-  public function update():bool {
-    return true;
+  public function updatePastie() {}
+
+  public function deletePastie() {}
+  
+  private function validateFormData(array $formData) : bool {
+    foreach($formData as $key => $field) {
+      if (!key_exists($key, self::VALID_FIELDS)) return false;
+
+      return true;
+    }
   }
 }
