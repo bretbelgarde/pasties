489dd023928a889cf2630db54e64058d8d99bbaf
Bret Belgarde <bretbelgarde@gmail.com>
2023-01-24 15:19:14 -0500

    Delete empty js file after removing prism.js

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-Prism.plugins.NormalizeWhitespace.setDefaults({
-	'remove-trailing': true,
-	'remove-indent': true,
-	'left-trim': true,
-	'right-trim': true,
-	// 'indent': 4,
-	'tabs-to-spaces': 2,
-});
